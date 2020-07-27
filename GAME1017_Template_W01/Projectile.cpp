089a99dc7827d4adf0387d1a953a330dd43acc29
boulanger14 <43892813+boulanger14@users.noreply.github.com>
2020-07-27 10:50:37 -0400

    minor changes to the projectile

--- GAME1017_Template_W01/Projectile.cpp
+++ GAME1017_Template_W01/Projectile.cpp
@@ -1,20 +1,34 @@
 #include "Projectile.h"
 #include "Sprite.h"
 #include "Engine.h"
+#include "MathManager.h"
 
 
 Projectile::Projectile(SDL_Rect src, SDL_FRect dst, SDL_Renderer* r, SDL_Texture* t) :
-	Sprite(src, dst, r, t) {}
+	Sprite(src, dst, r, t) {};
+
+Projectile::Projectile(bool fromPlayer, SDL_FPoint origin, double angle, float size, SDL_Texture* t)
+{
+	m_pRend = Engine::Instance().GetRenderer();
+	m_src = { 0,0,36,36 };
+	m_dst = { origin.x, origin.y, 32*size, 32* size };
+	m_pText = t;
+	owner = (fromPlayer) ? PLAYER_BULLET : ENEMY_BULLET;
+
+	velX = cos(MathManager::Deg2Rad(angle));
+	velY = -sin(MathManager::Deg2Rad(angle));
+	m_size = size;
+}
+
 
 Projectile::~Projectile()
 {
-
 }
 
 void Projectile::update()
 {
-	m_dst.x += velX;
-	m_dst.y += velY;
+	m_dst.x += velX * m_speed / m_size;
+	m_dst.y += velY * m_speed / m_size;
 }
 
 void Projectile::render()
